9cd158421f6245fa037093e510b0081256974613
Acer <mmm765432@gmail.com>
2021-03-29 20:11:35 +0300

    hw1.1

--- app/src/main/java/com/example/homework1/MainActivity.java
+++ app/src/main/java/com/example/homework1/MainActivity.java
@@ -10,7 +10,6 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recycler_container);
         if(savedInstanceState == null)
